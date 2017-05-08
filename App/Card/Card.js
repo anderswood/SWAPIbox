0c996f5157826d7353eb076fef7233135a84699f
Anders Wood <anderswood@gmail.com>
2017-05-08 14:59:44 -0600

    setup add

---
+++ App/Card/Card.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Card = ( {cardArr} ) => {
+  console.log(cardArr);
+  return(
+    <div></div>
+  )
+
+
+}
+
+Card.propTypes = {
+  cardArr: PropTypes.array
+}
+
+export default Card
