2ef2fb13885ea3cab31e912793b0a8e66dfe5292
Anders Wood <anderswood@gmail.com>
2017-05-08 23:06:33 -0600

    update css and add buttons

--- App/Button/Button.js
+++ App/Button/Button.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = ( {cardArr} ) => {
-  console.log(cardArr);
+const Button = ( {type} ) => {
+
   return(
-    <div></div>
+    <button>{ type }</button>
   )
 
 
 }
 
 Button.propTypes = {
-  cardArr: PropTypes.array
+  type: PropTypes.string
 }
 
 export default Button
