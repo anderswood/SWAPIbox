4164993d0981c3697a3e7f396e4c9a94955a40d3
Anders Wood <anderswood@gmail.com>
2017-05-10 11:08:59 -0600

    update cardArr

--- App/Scrubbers/planetsNeedPeople.js
+++ App/Scrubbers/planetsNeedPeople.js
@@ -1,8 +1,9 @@
 const planetsNeedPeople = (planets, people) => {
-  let updatedPlanets = Object.assign({}, planets)
-  people.map((person, index) => {
-    updatedPlanets[person.PlanetUrl]
-      .Residents[updatedPlanets[person.PlanetUrl].Residents.length] = person.Name
+  let updatedPlanets = planets
+
+ people.forEach((person, index) => {
+    updatedPlanets[person.PlanetUrl].Residents
+      [updatedPlanets[person.PlanetUrl].Residents.length] = person.Name
   })
   return updatedPlanets
 }
